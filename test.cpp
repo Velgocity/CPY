01662c54de5c274e781cfed834978e49b5232d41
Velgo <99982413+Velgocity@users.noreply.github.com>
2022-03-15 20:15:16 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,20 +1,26 @@
-#include <iostream>
-#include "lib/console/console.hpp"
+#include "lib/console/std.hpp"
 
-sng nmspc std;
+stdlib;
 
-nt hello() {
-    return 1;
+nt loo(nt a, nt b) {
+    return a + b;
 }
 
-nt mn() {
-    prnt("Hello World");
-    str a = input("Lütfen adınızı giriniz : ");
-    prnt("Adınız : " + a);
+vd loopN() {
+    prnt("hello");
+}
 
-    // for
-    fr (nt i = 0;i < 10; ++i) {
-        string ia = InttoString(i);
-        prnt(ia);
+vd loopT() {
+    prnt("a.")
+}
+
+nt main(arg) {
+    nt i;
+    fr (i=0;i<10;++i) {
+        str ConvertString = InttoString(i);
+        prnt(ConvertString);
     }
+    loopNumber(loopN, 5);
+    loopTime(loopT, 1000);
+
 }
